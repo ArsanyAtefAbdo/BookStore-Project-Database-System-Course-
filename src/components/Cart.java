fe52177aad0adc37d152e9a3ad980e6a26371e73
DELL <programmergood37@gmail.com>
2020-05-16 21:54:28 +0200

    modify the Cart

--- src/components/Cart.java
+++ src/components/Cart.java
@@ -4,25 +4,63 @@ import java.util.ArrayList;
 
 public class Cart {
 
-	public ArrayList<IBook> book;
+	private ArrayList<IBook> books;
 	
-	public void add_items() {
+
+	/**
+	 * @param book to be added
+	 */
+	public void add_items(IBook book) {
+		this.books.add(book);
+	}
+	/**
+	 * @param index of item.
+	 * @return the price of this item.
+	 */
+	public int get_item_price(int index) {
 		
+		if(index < 0 || this.books.isEmpty()) {
+			return 0;
+		}
+		return this.books.get(index).getPrice();
 	}
 	
-	public void view_items() {
-		
+	/**
+	 * @return the total price of all items.
+	 */
+	public int get_total_price() {
+		int total = 0;
+		for(IBook book: this.books) {
+			total += book.getPrice();
+		}
+		return total;
 	}
 	
-	public void view_price() {
+	/**
+	 * @param index of item to be removed.
+	 * @return true if the item is removed successfully.
+	 */
+	public boolean remove_item(int index) {
 		
+		if(index < 0 || this.books.isEmpty()) {
+			return false;
+		}
+		
+		this.books.remove(index);
+		return true;
 	}
 	
-	public void remove_item() {
-		
+	/**
+	 * @return the books
+	 */
+	public ArrayList<IBook> getBooks() {
+		return books;
 	}
-	
-	public void check_credit() {
-		
+
+	/**
+	 * @param books the books to set
+	 */
+	public void setBooks(ArrayList<IBook> books) {
+		this.books = books;
 	}
 }
